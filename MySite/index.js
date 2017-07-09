88dd95a813328f2a9ee2386a6be6bec5e1a8fcf0
Patrick O'Reilly <paddy1234@hotmail.com>
2017-07-09 13:06:07 +0100

    Update index.js
    Changes recomended by JSlint

--- MySite/index.js
+++ MySite/index.js
@@ -1,4 +1,5 @@
-/*JavaScript that adds a class to the header element; slightly minimizes the header bar upon scrolling down 500pixels. It also makes the background color slightly transparrent */
+/*JavaScript that adds a class to the header element; slightly minimizes the header bar upon 
+scrolling down 500pixels. It also makes the background color slightly transparrent */
 
 function init() {
     window.addEventListener('scroll', function () {
@@ -12,22 +13,23 @@ function init() {
     });
 }
 
-/*JavaScript that adds a class to the header and fullpage elements; offsets them by 170pixels to display the offscreen navigation underneath upon clicking the navigation button - Three lines, which appear when the screen width is less than 875pixels  */
+/*JavaScript that adds a class to the header and fullpage elements; offsets them by 170pixels 
+to display the offscreen navigation underneath upon clicking the navigation button - Three lines, 
+which appear when the screen width is less than 875pixels  */
 
 function onClickHandler(checkbox) {
     if (checkbox.checked) {
         document.getElementById("pagehead").classList.add('nav_open');
         document.getElementById("fullpage").classList.add('nav_open');
-    }
-    else {
+    } else {
         if (document.getElementById("pagehead").classList.contains('nav_open')) {
-            document.getElementById("pagehead").classList.remove('nav_open')
+            document.getElementById("pagehead").classList.remove('nav_open');
         }
         if (document.getElementById("fullpage").classList.contains('nav_open')) {
-            document.getElementById("fullpage").classList.remove('nav_open')
+            document.getElementById("fullpage").classList.remove('nav_open');
         }
     }
         
-};
+}
 
 window.onload = init();
